5ab271dedeb96bf9b7c7f87f31ece11005c57a05
Milos Zivlak <zivlakmilos@gmail.com>
2023-08-22 01:44:57 +0200

    Open, close handshake

--- src/__tests__/sim800.ts
+++ src/__tests__/sim800.ts
@@ -5,12 +5,32 @@ const CONFIG = {
   baudRate: 115200,
 }
 
-test('test SIM800 Instantiation', async () => {
+describe('test SIM800', () => {
   const sim800 = new SIM800(PORT, CONFIG);
 
-  const isOpen = await sim800.open();
-  expect(isOpen).toBe(true);
-  expect(sim800.isOpen()).toBe(true);
+  test('test SIM800 Instantiation', async () => {
+    const isOpen = await sim800.open();
+    expect(isOpen).toBe(true);
+    expect(sim800.isOpen()).toBe(true);
+  });
 
-  sim800.close();
+  test('test SIM800 Handshake', async () => {
+    const handshake = await sim800.sendHandshake();
+    expect(handshake).toBe(true);
+  });
+
+  test('test SIM800 Send SMS', async () => {
+  });
+
+  test('test SIM800 List SMS', async () => {
+  });
+
+  test('test SIM800 Read SMS', async () => {
+  });
+
+  test('test SIM800 Close', async () => {
+    const isClosed = await sim800.close();
+    expect(isClosed).toBe(true);
+    expect(sim800.isOpen()).toBe(false);
+  });
 });
--- src/sim800.ts
+++ src/sim800.ts
@@ -1,17 +1,43 @@
-import { SerialPort } from "serialport";
+import { ReadlineParser, SerialPort } from "serialport";
 
 export type SIM800Options = {
   baudRate?: number,
 }
 
+type Queue = {
+  data: string,
+  callback: (data: string) => void,
+}
+
 class SIM800 {
   private tty: SerialPort;
+  private parser: ReadlineParser;
+
+  private queue: Queue[] = [];
+  private current: Queue | undefined = undefined;
 
   constructor(private port: string, private options: SIM800Options | undefined = undefined) {
     this.tty = new SerialPort({
       path: port,
       baudRate: options?.baudRate || 9600,
       autoOpen: false,
+      endOnClose: true,
+    });
+
+    this.parser = this.tty.pipe(new ReadlineParser({ delimiter: '\r\n', }));
+
+    this.parser.on('data', data => {
+      const trimData = data.trim();
+
+      if (!this.current) {
+        return;
+      }
+      if (this.current.data === trimData) {
+        return;
+      }
+
+      this.current.callback(trimData);
+      this.processQueue();
     });
   }
 
@@ -32,12 +58,48 @@ class SIM800 {
     return this.tty.isOpen;
   }
 
-  close(): void {
-    this.tty.close();
+  close(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.tty.close(err => {
+        if (err) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
   }
 
   sendMessage(phone: string, txt: string): boolean {
     return false;
+  }
+
+  sendHandshake(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      this.queue.push({
+        data: 'AT',
+        callback: (data: string) => {
+          if (data === 'OK') {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        }
+      })
+      if (!this.current) {
+        this.processQueue();
+      }
+    });
+  }
+
+  private processQueue() {
+    const next = this.queue.shift();
+    this.current = next;
+    if (!next) {
+      return;
+    }
+
+    this.tty.write(next.data + '\r\n');
   }
 }
 
